090cfca7767fdece45e4b98f49811f5c9a3e7885
YaniaNada <166074902+YaniaNada@users.noreply.github.com>
2025-01-06 14:31:42 +0100

    dimensional_data_modeling_day_3a

---
+++ 1_dimensional_data_modeling/lab_3a_building_graph_model.sql
@@ -0,0 +1,33 @@
+
+-- Creating the table schema for graph model --
+
+CREATE TYPE vertex_type 
+	AS ENUM('player', 'team', 'game');
+
+
+CREATE TABLE vertices (
+	identifier TEXT,
+	type vertex_type,
+	properties JSON,
+	PRIMARY KEY (identifier, type)
+);
+
+CREATE TYPE edge_type 
+	AS ENUM('plays_against', 
+			'shares_team', 
+			'plays_in',
+			'plays_on');
+
+CREATE TABLE edges (
+	-- In some cases 'edge_id TEXT' is also used
+	subject_identifier TEXT,
+	subject_type vertex_type,
+	object_identifier TEXT,
+	object_type vertex_type,
+	edge_type edge_type,
+	properties JSON,
+	PRIMARY KEY (subject_identifier, 
+				subject_type,
+				object_identifier,
+				object_type,
+				edge_type))
